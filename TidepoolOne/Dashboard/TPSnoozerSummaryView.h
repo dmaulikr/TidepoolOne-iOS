fe0db9fd002d643953a72cc8026cd9f8118bc014
e7mac <mayank.ot@gmail.com>
2013-09-12 17:01:58 -0700

    moved snoozer summary to different view.. haven't integrated new view into dashboard yet

--- TidepoolOne/Dashboard/TPSnoozerSummaryView.h
+++ TidepoolOne/Dashboard/TPSnoozerSummaryView.h
@@ -1,17 +1,16 @@
 //
-//  TPDashboardHeaderView.h
+//  TPSnoozerSummaryView.h
 //  TidepoolOne
 //
-//  Created by Mayank Sanganeria on 8/23/13.
+//  Created by Mayank Sanganeria on 9/12/13.
 //  Copyright (c) 2013 Mayank Sanganeria. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "TPBarGraphView.h"
 #import "TPCurveGraphView.h"
-#import "TPSnoozerSummaryView.h"
 
-@interface TPDashboardHeaderView : UIView
+@interface TPSnoozerSummaryView : UIView
 
 @property (weak, nonatomic) IBOutlet TPLabelBold *dailyBestLabel;
 @property (weak, nonatomic) IBOutlet TPLabelBold *allTimeBestLabel;
@@ -20,13 +19,5 @@
 
 @property (strong, nonatomic) NSArray *densityData;
 @property (strong, nonatomic) NSArray *results;
-
-
-@property (weak, nonatomic) IBOutlet UIScrollView *fitbitScrollView;
-@property (weak, nonatomic) IBOutlet TPBarGraphView *fitbitBarGraphView;
-@property (weak, nonatomic) IBOutlet TPCurveGraphView *fitbitSleepGraphView;
-@property (weak, nonatomic) IBOutlet TPCurveGraphView *fitbitActivityGraphView;
-
--(void)dismissPopovers;
 
 @end
